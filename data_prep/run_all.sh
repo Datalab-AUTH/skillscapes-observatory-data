ed7863c7047babf4aa4f4a26e714785310da83e6
George Vlahavas <vlahavas@gmail.com>
2025-11-07 01:02:23 +0200

    Mount /tmp, export env vars

--- data_prep/run_all.sh
+++ data_prep/run_all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-RSCRIPT_CMD="docker run --rm -ti -u `id -u`:`id -g` -v `pwd`:/app r-skillscapes Rscript"
+RSCRIPT_CMD="docker run --rm -ti -u `id -u`:`id -g` -v `pwd`:/app -v /tmp:/tmp r-skillscapes Rscript"
 
 if [ -f env ]; then
-	source env
+	for line in env; do
+		export $line
+	done
 fi
 
 ${RSCRIPT_CMD} all.R
