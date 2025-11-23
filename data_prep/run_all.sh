23456d962ce03e983fea2920752adee7c7b1551e
George Vlahavas <vlahavas@gmail.com>
2025-11-23 22:05:00 +0200

    Merge INSETE script into main script

--- data_prep/run_all.sh
+++ data_prep/run_all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#
+# All these are for the Eurostat and Aegean DB data
+# 
+
 RSCRIPT_CMD="docker run --rm -ti -u `id -u`:`id -g` -v `pwd`:/app -v /tmp:/tmp r-skillscapes Rscript"
 
 if [ -f env ]; then
@@ -9,3 +13,20 @@ if [ -f env ]; then
 fi
 
 ${RSCRIPT_CMD} all.R
+
+#
+# Now for the INSETE stuff
+#
+
+# First, preprocess the excel files to make the csv files
+cd INSETE
+docker run --rm -ti -u `id -u`:`id -g` -v `pwd`:/app -v /tmp:/tmp \
+	python-skillscapes \
+	/bin/sh -c 'python data_INSETE_hotels_preprocess.py && \
+		python data_INSETE_short_stay_preprocess.py && \
+		python data_INSETE_employment.py && \
+		python data_INSETE_key_figures_preprocess.py && \
+		python data_INSETE_STR_preprocess.py'
+
+cd -
+
