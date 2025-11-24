a432c37f7910e0a34a468187e02611a0c7e70983
George Vlahavas <vlahavas@gmail.com>
2025-11-24 12:31:07 +0200

    Update docker image names

--- data_prep/run_all.sh
+++ data_prep/run_all.sh
@@ -4,7 +4,7 @@
 # All these are for the Eurostat and Aegean DB data
 # 
 
-RSCRIPT_CMD="docker run --rm -ti -u `id -u`:`id -g` -v `pwd`:/app -v /tmp:/tmp r-skillscapes Rscript"
+RSCRIPT_CMD="docker run --rm -ti -u `id -u`:`id -g` -v `pwd`:/app -v /tmp:/tmp datalabauth/skillscapes-r Rscript"
 
 if [ -f env ]; then
 	for line in env; do
@@ -19,14 +19,12 @@ ${RSCRIPT_CMD} all.R
 #
 
 # First, preprocess the excel files to make the csv files
-cd INSETE
 docker run --rm -ti -u `id -u`:`id -g` -v `pwd`:/app -v /tmp:/tmp \
-	python-skillscapes \
-	/bin/sh -c 'python data_INSETE_hotels_preprocess.py && \
+	datalabauth/skillscapes-python \
+	/bin/sh -c 'cd INSETE; \
+		python data_INSETE_hotels_preprocess.py && \
 		python data_INSETE_short_stay_preprocess.py && \
 		python data_INSETE_employment.py && \
 		python data_INSETE_key_figures_preprocess.py && \
 		python data_INSETE_STR_preprocess.py'
-
-cd -
 
