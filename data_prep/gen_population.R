0e5a582c90c50ba2efdf2c4ad7c9515e41035c4d
George Vlahavas <vlahavas@gmail.com>
2025-12-19 14:05:58 +0200

    Add NUTS3 data for population

--- data_prep/gen_population.R
+++ data_prep/gen_population.R
@@ -9,12 +9,12 @@ if (!exists('con_sqlite')) {
   con_sqlite <- dbConnect(RSQLite::SQLite(), "skillscapes.sqlite")
 }
 
-d_gen_population <- get_eurostat('demo_r_d2jan',
-                  time_format = "num", stringsAsFactors = TRUE) |>
+d_gen_population_nuts2 <- get_eurostat('demo_r_d2jan',
+                                 time_format = "num", stringsAsFactors = TRUE) |>
   filter(
     sex == "T",
     TIME_PERIOD >= 2008
-    ) |>
+  ) |>
   select(-freq, -unit) |>
   rename("year" = "TIME_PERIOD") |>
   pivot_wider(names_from = age, values_from = values) |>
@@ -25,6 +25,30 @@ d_gen_population <- get_eurostat('demo_r_d2jan',
   mutate(
     year = as.integer(year)
   )
+
+d_gen_population_nuts3 <- get_eurostat('demo_r_pjangrp3',
+                  time_format = "num", stringsAsFactors = TRUE) |>
+  filter(
+    sex == "T",
+    age == c("TOTAL", "Y15-19", "Y20-24", "Y25-29"),
+    TIME_PERIOD >= 2008
+    ) |>
+  select(-freq, -unit, -sex) |>
+  rename("year" = "TIME_PERIOD") |>
+  pivot_wider(names_from = age, values_from = values) |>
+  replace_na(list(`Y15-19` = 0, `Y20-24` = 0, `Y25-29` = 0 )) |>
+  mutate(
+    population_15_29 = `Y15-19` + `Y20-24` + `Y25-29`,
+    population_15_29 = na_if(population_15_29, 0)
+    ) |>
+  rename("population_total" = "TOTAL") |>
+  select(-starts_with('Y', ignore.case=F)) |>
+  mutate(
+    year = as.integer(year)
+  ) |>
+  filter(str_length(geo) == 5) # only NUTS3
+
+d_gen_population <- rbind(d_gen_population_nuts2, d_gen_population_nuts3)
 
 dbWriteTable(con_sqlite, "gen_population", d_gen_population, overwrite = TRUE)
 
