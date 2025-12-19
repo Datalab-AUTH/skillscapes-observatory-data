9ba4ba6881aa29519829b7d7053d22b928ce279b
George Vlahavas <vlahavas@gmail.com>
2025-12-19 16:22:35 +0200

    Calculate involuntary employment percentages

--- data_prep/eu_labour_precarity_involuntary_employment.R
+++ data_prep/eu_labour_precarity_involuntary_employment.R
@@ -8,11 +8,22 @@ if (!exists('con_sqlite')) {
   con_sqlite <- dbConnect(RSQLite::SQLite(), "skillscapes.sqlite")
 }
 
+if (!exists('d_permanent_temporary_employment')) {
+  source('eu_labour_permanent_temporary_employment.R')
+}
+
 d_involuntary_part_time_employment <- read_csv("involuntary_employment/NUTS2_Involuntary_Part_Time_Employment.csv")
 d_involuntary_temporary_employment <- read_csv("involuntary_employment/NUTS2_Involuntary_Temporary_Employment.csv")
 
 d_involuntary_employment <- d_involuntary_part_time_employment |>
   full_join(d_involuntary_temporary_employment, by=c("geo", "year")) |>
-  mutate(year = as.integer(year))
+  mutate(year = as.integer(year)) |>
+  left_join(d_permanent_temporary_employment, by=c("geo", "year")) |>
+  left_join(d_eu_labour_part_full_time_employment, by=c("geo", "year")) |>
+  mutate(
+    involuntary_temporary_pct = 100 * involuntary_temporary / temporary_employment,
+    involuntary_part_time_pct = 100 * involuntary_part_time / employment_part_time
+  ) |>
+  select(geo, year, starts_with("involuntary"))
 
 dbWriteTable(con_sqlite, "eu_labour_precarity_involuntary_employment", d_involuntary_employment, overwrite = TRUE)
