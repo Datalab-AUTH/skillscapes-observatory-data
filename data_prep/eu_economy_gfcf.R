ad5f38cdeadcbb9bb415cfff10eb4ed9a09bbc0a
George Vlahavas <vlahavas@gmail.com>
2026-03-26 17:35:02 +0200

    Add all sectors to GFCF

--- data_prep/eu_economy_gfcf.R
+++ data_prep/eu_economy_gfcf.R
@@ -10,21 +10,35 @@ if (!exists('con_sqlite')) {
 }
 
 d_eu_economy_gfcf <- get_eurostat('nama_10r_2gfcf',
-                  filters = list(
-                    currency = 'MIO_EUR', # million euros
-                    nace_r2 = 'TOTAL', # all NACE activities
-                    sector = 'S1' # Total economy
-                  ),
-                  time_format = "num", stringsAsFactors = TRUE) |>
-  select(-freq, -nace_r2, -sector, -currency) |>
+                                  filters = list(
+                                    currency = "MIO_EUR",
+                                    sector = "S1"
+                                  ),
+                                  time_format = "num", stringsAsFactors = TRUE) |>
+  select(-freq, -currency, -sector) |>
+  pivot_wider(names_from = nace_r2, values_from = values) |>
   rename(
     "year" = "time",
-    "gfcf" = "values") |>
-  filter(year >= 2008) |>
+    "gfcf" = "TOTAL",
+    "gcfc_sector_a" = "A",
+    "gcfc_sector_bcde" = "B-E",
+    "gcfc_sector_c" = "C",
+    "gcfc_sector_f" = "F",
+    "gcfc_sector_ghij" = "G-J",
+    "gcfc_sector_ghi" = "G-I",
+    "gcfc_sector_j" = "J",
+    "gcfc_sector_klmn" = "K-N",
+    "gcfc_sector_k" = "K",
+    "gcfc_sector_l" = "L",
+    "gcfc_sector_mn" = "M_N",
+    "gcfc_sector_opqrstu" = "O-U",
+    "gcfc_sector_opq" = "O-Q",
+    "gcfc_sector_rstu" = "R-U"
+  ) |>
   mutate(
-    year = as.integer(year),
-    gfcf = as.numeric(gfcf)
-  )
+    year = as.integer(year)
+  ) |>
+  filter(year >= 2008)
 
 dbWriteTable(con_sqlite, "eu_economy_gfcf", d_eu_economy_gfcf, overwrite = TRUE)
 
